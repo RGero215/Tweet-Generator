2d63fc98feee7f63fdbc71dcf4c583dec235d4cf
Ramon Geronimo <rgero215@gmail.com>
2019-01-27 11:22:44 -0800

    Add comments to rearrange func

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -1,7 +1,9 @@
 import sys
-
+#.1 define a function that will take argumentes from the command line 
 def rearrange():
+    #.2 starting from the second argument to avoid files name
     command_line_arguments_list = sys.argv[1:] 
+    #.3 return the arguments arrange in alphabetical order
     return ' '.join(sorted(command_line_arguments_list))
 
 if __name__ == "__main__":
